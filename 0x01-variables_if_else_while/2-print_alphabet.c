195d47415ecfb10449763c8a55a67cf3940c3b39
Naolak <90989814+Naolak@users.noreply.github.com>
2022-03-15 11:52:45 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+   * main - print alphabet in lowercase
+   *
+   *Return: zero
+   */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
