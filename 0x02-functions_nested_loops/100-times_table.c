3ad4b743bc7621c0d33b14d9bd34fdf32424c602
Naolak <90989814+Naolak@users.noreply.github.com>
2022-03-18 15:27:55 +0300

    Time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,13 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * n: size of time table
+ * @n: size of time table
+ *
  */
 void print_times_table(int n)
 {
 	int a, b, c;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
@@ -17,7 +19,7 @@ void print_times_table(int n)
 				if (b == 0)
 				{
 					_putchar(c + '0');
-				} 
+				}
 				else if (c < 10 && b != 0)
 				{
 					_putchar(',');
@@ -33,7 +35,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
-				} 
+				}
 				else if (c >= 100)
 				{
 					_putchar(',');
