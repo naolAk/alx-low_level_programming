7c4dfbebbebce6867d007c0be0ac8582cce0a54a
Naolak <90989814+Naolak@users.noreply.github.com>
2022-04-17 00:40:46 +0300

    Initialize type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,13 +8,30 @@
  * @age: age to initialize
  * @owner: owner to initialize
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if( d == NULL)
+	if (d == NULL)
 		d = malloc(sizeof(struct dog));
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
